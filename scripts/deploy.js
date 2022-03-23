46c129a71639f86ebb8259c63d92cd15dcb806f5
mr-abims <adebayoabimbola9@gmail.com>
2022-03-24 00:45:22 +0100

    deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  const [owner] = await hre.ethers.getSigners();
+  const BankContractFactory = await hre.ethers.getContractFactory("Bank");
+  const bank = await BankContractFactory.deploy();
+
+  await bank.deployed();
+
+  console.log("Bank Deployed to:", bank.address);
+  console.log("BankContract owner address:", owner.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
